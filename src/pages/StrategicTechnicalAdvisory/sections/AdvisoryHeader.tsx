9fdeb5065c3b1b83048b9aaf33d30598ca905914
webdeveloperimpulsedigital-commits <webdeveloper.impulsedigital@gmail.com>
2026-05-26 13:00:25 +0530

    chnages in files error fix

--- src/pages/StrategicTechnicalAdvisory/sections/AdvisoryHeader.tsx
+++ src/pages/StrategicTechnicalAdvisory/sections/AdvisoryHeader.tsx
@@ -31,7 +31,7 @@ const AdvisoryHeader = () => {
             opacity: 1,
             y: 0,
             rotateX: 0,
-            transition: { duration: 0.8, ease: [0.215, 0.61, 0.355, 1] }
+            transition: { duration: 0.8, ease: [0.215, 0.61, 0.355, 1] as [number, number, number, number] }
         }
     };
 
